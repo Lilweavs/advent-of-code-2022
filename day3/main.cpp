0ca3571e8ddc17ccb0633282472bcabc81e3b209
LilWeavs <andrewjw827@gmail.com>
2022-12-03 15:42:44 -0600

    day3: optimizations

--- day3/main.cpp
+++ day3/main.cpp
@@ -5,6 +5,7 @@
 #include <regex>
 #include <map>
 #include <set>
+#include <array>
 
 #include "clock.h"
 
@@ -12,60 +13,61 @@ using namespace std;
 
 Timer timer;
 
-int sumChars(vector<char> &vec) {
-    int sum = 0;
-    for (auto v : vec) {
-        if (v >= 'a')
-            sum += (v - 96);
-        else
-            sum += (v - 38);
-    }   
-    return sum;
+constexpr array<int, 128> MakeTable() {
+    array<int, 128> arr{};
+    for (int i = 'A'; i <= 'Z'; i++)
+        arr[i] = i - 38;
+    for (char i = 'a'; i <= 'z'; i++)
+        arr[i] = i - 96;
+    return arr;
 }
 
-// int tmp1[52], tmp2[52];
+constexpr auto table = MakeTable();
 
 int main(int argc, char* argv[]) {
 
     timer.tick();
-
-    ifstream ifile("input.txt", std::ios::in);
-
     string line;
-    vector<char> rucksack;
-    vector<char> intersection;
-
+    vector<string> rucksack;
+    ifstream ifile("input.txt", std::ios::in);
     while (getline(ifile, line, '\n')) {
-        set<char> pack1(line.begin(), line.end() - line.length() / 2);
-        set<char> pack2(line.begin() + line.length() / 2, line.end());
-        set_intersection(pack1.begin(), pack1.end(), pack2.begin(), pack2.end(), back_inserter(intersection));
+        rucksack.push_back(line);
     }
     ifile.close();
 
-    int ans1 = sumChars(intersection);
+    cout << "Input Parsing: " << timer.tock() / 1000.0 << "ms" << endl;
 
-    vector<set<char>> rucksacks;
-    ifile.open("input.txt", std::ios::in);
-    while (getline(ifile, line, '\n')) {
-        set<char> pack(line.begin(), line.end());
-        rucksacks.push_back(pack);
+    /* Part 1 Original Optimized */
+    timer.tick();
+    vector<char> common;
+    int intersectionSum = 0;
+    for (auto& rs: rucksack) {
+        int middle = rs.length() / 2;
+        sort(rs.begin(), rs.end() - middle);
+        sort(rs.begin() + middle, rs.end());
+        set_intersection(rs.begin(), rs.end() - middle, rs.begin() + middle, rs.end(), back_inserter(common));
+        intersectionSum += table[common[0]];
+        common.clear();
     }
-    ifile.close();
-
-    vector<char> badges;
-    for (int i = 0; i < static_cast<int>(rucksacks.size()); i+=3) {
-        vector<char> tmp;
-        set_intersection(rucksacks[i].begin(), rucksacks[i].end(), rucksacks[i+1].begin(), rucksacks[i+1].end(), back_inserter(tmp));
-        set_intersection(tmp.begin(), tmp.end(), rucksacks[i+2].begin(), rucksacks[i+2].end(), back_inserter(badges));
-    }
-
-    int ans2 = sumChars(badges);
-
-    timer.tock();
-
+    
     cout << "Time Elapsed: " << timer.tock() / 1000.0 << "ms" << endl;
-    cout << "Part 1: " << ans1 << endl;
-    cout << "Part 2: " << ans2 << endl;
+    cout << "Part 1: " << intersectionSum << endl;
+    
+    /* Part 2 Old Optimized */
+    timer.tick();
+    for (auto& rs : rucksack)
+        sort(rs.begin(), rs.end());
+
+    intersectionSum = 0;
+    for (size_t i = 0; i < rucksack.size(); i+=3) {
+        set_intersection(rucksack[i].begin(), rucksack[i].end(), rucksack[i+1].begin(), rucksack[i+1].end(), back_inserter(common));
+        set_intersection(common.begin(), common.end(), rucksack[i+2].begin(), rucksack[i+2].end(), back_inserter(common));
+        intersectionSum += table[common.back()];
+        common.clear();
+    }
+
+    cout << "Part 2 Elapsed: " << timer.tock() / 1000.0 << "ms" << endl;
+    cout << "Part 2: " << intersectionSum << endl;
 
     return 0;
 }   
