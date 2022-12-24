f0661b89d119e6e8a554983bef49e1ea8a024976
LilWeavs <andrewjw827@gmail.com>
2022-12-23 21:45:37 -0600

    day 20 part 2 and 1 refactor

--- day20/main.cpp
+++ day20/main.cpp
@@ -10,23 +10,21 @@
 using namespace std;
 
 struct List {
-    List(int a) : value(a) { };
+    List(int64_t a) : value(a) { };
+    List() { };
     List* left;
     List* right;
-    int value;
+    int64_t value;
 };
 
-
-int findEncryptionNumber(List* l, int numAfter) {
+int64_t findEncryptionNumber(List* l, int numAfter) {
 
     List* ptr = l;
     for (int i = 0; i < numAfter; i++)
         ptr = ptr->right;
 
-    cout << ptr->value << endl;;
     return ptr->value;
-}
-
+};
 
 Timer timer;
 
@@ -35,17 +33,16 @@ int main(int argc, char* argv[]) {
     timer.tick();
 
     string line;
-    vector<int> sequence;
-    vector<int> mixedSequence;
+    // vector<int64_t> sequence;
+    // vector<int64_t> mixedSequence;
 
     vector<List*> shuffleOrder;
-    List* listMiddle;
+    List* listMiddle = new List(0);
 
     ifstream ifile("input.txt", std::ios::in);
-    // while (getline(ifile, line)) { sequence.push_back(stoi(line)); }
     int i = 0;
     while (getline(ifile, line)) {
-        int value = stoi(line);
+        int64_t value = stoll(line);
         shuffleOrder.push_back(new List(value));
         if (value == 0) { listMiddle = shuffleOrder.back(); }
         if (i) {
@@ -59,7 +56,9 @@ int main(int argc, char* argv[]) {
 
     ifile.close();
 
+    cout << "Input Parsing: " << timer.tock() / 1000.0 << "ms" << endl;
 
+    timer.tick();
     for (const auto& num : shuffleOrder){
 
         int shuffleCount = num->value;
@@ -105,17 +104,7 @@ int main(int argc, char* argv[]) {
     //     ptr = ptr->right;
     // } while (ptr != shuffleOrder.front()->right);
 
-
-    // while (ptr != shuffleOrder.front()) {
-    //     cout << ptr->value << " " << endl;
-    //     ptr = ptr->right;
-    // }
-
-    
-    // cout << findEncryptionNumber(listMiddle, 1000) << endl;
-    // cout << findEncryptionNumber(listMiddle, 2000) << endl;
-    // cout << findEncryptionNumber(listMiddle, 3000) << endl;
-    int sum = 0;
+    int64_t sum = 0;
     for (int i = 1; i < 4; i++)
         sum += findEncryptionNumber(listMiddle, 1000*i);
 
@@ -123,60 +112,80 @@ int main(int argc, char* argv[]) {
     cout << "Part 1: " << sum << endl;
 
 
-
     for (auto& ptr : shuffleOrder)
         delete ptr;
     shuffleOrder.clear();
 
+    ifile.open("input.txt", std::ios::in);
+    i = 0;
+    while (getline(ifile, line)) {
+        int64_t value = stoll(line) * 811589153;
+        shuffleOrder.push_back(new List(value));
+        if (value == 0) { listMiddle = shuffleOrder.back(); }
+        if (i) {
+            shuffleOrder[i]->left = shuffleOrder[i-1];
+            shuffleOrder[i-1]->right = shuffleOrder[i]; 
+        }
+        i++;
+    }
+    shuffleOrder.front()->left = shuffleOrder.back();
+    shuffleOrder.back()->right = shuffleOrder.front();
 
-    // mixedSequence = sequence;
+    ifile.close();
 
-    // int mod = sequence.size();
-    // for (size_t i = 0; i < sequence.size(); i++) {
-    //     int tmp = sequence[i];
+    timer.tick();
+    for (int k = 0; k < 10; k++) {
+        for (const auto& num : shuffleOrder){
 
-    //     auto it = find(mixedSequence.begin(), mixedSequence.end(), tmp);
-    //     int index = distance(mixedSequence.begin(), it);
-        
-    //     if (tmp < 0) {
-    //         // go backwards
-            
-    //         int l = (index + tmp) % mod ;
-            
-    //         if (l <= 0) {
-    //             for (int j = index; j < mod + l - 1 ; j++) {
-    //                 swap(mixedSequence[j], mixedSequence[j+1]);
-    //             }
-    //         } else {
-    //             for (int j = index; j > l ; j--) {
-    //                 swap(mixedSequence[j], mixedSequence[j-1]);
-    //             }
-    //         }            
-    //     } else {
-    //         int l = (index + tmp) % mod ;
-            
-    //         if (l < tmp) {
-    //             for (int j = index; j > l + 1 ; j--) {
-    //                 swap(mixedSequence[j], mixedSequence[j-1]);
-    //             }
-    //         } else {
-    //             for (int j = index; j < l ; j++) {
-    //                 swap(mixedSequence[j], mixedSequence[j+1]);
-    //             }
-    //         }          
-    //     }
-    // }
+            int shuffleCount = (num->value % static_cast<int64_t>(shuffleOrder.size() - 1));
 
-    // auto it = find(mixedSequence.begin(), mixedSequence.end(), 0);
-    // int zeroindex = distance(mixedSequence.begin(), it);
-    
-    // int ans = mixedSequence[(zeroindex + 1000) % mod] + mixedSequence[(zeroindex + 2000) % mod] + mixedSequence[(zeroindex + 3000) % mod];
-    // cout << mixedSequence[(zeroindex + 1000) % mod] << endl;
-    // cout << mixedSequence[(zeroindex + 2000) % mod] << endl;
-    // cout << mixedSequence[(zeroindex + 3000) % mod] << endl;
+            if (shuffleCount >= 0) {
+                for (int i = 0; i < shuffleCount; i++) {
+                    /* 1 2 -3 4 for 1*/
+                    List* lNum = num->left;           // lnum  = 4
+                    List* rNum = num->right;          // rnum  = 2
+                    List* rrNum = num->right->right;  // rrnum = -3
 
-    // cout << "Time Elapsed: " << timer.tock() / 1000.0 << "ms" << endl;
-    // cout << "Part 1: " << ans << endl;
+                    rrNum->left = num;
+                    rNum->right = num;
+
+                    rNum->left  = lNum; // same same
+                    lNum->right = rNum; // same same
+
+                    num->left  = rNum;
+                    num->right = rrNum;
+                }
+            } else {
+                for (int i = 0; i < -shuffleCount; i++) {
+                    /* 1 -1 2 4 for -1*/
+                    List* lNum = num->left;           // lnum  = 1
+                    List* rNum = num->right;          // rnum  = 2
+                    List* llNum = num->left->left;    // llNum = 4
+
+                    llNum->right = num;
+                    lNum->left   = num;
+
+                    lNum->right = rNum; // same same
+                    rNum->left  = lNum; // same same
+
+                    num->left  = llNum;
+                    num->right = lNum;
+                }
+            }
+        }
+    }
+
+    sum = 0;
+    for (int i = 1; i < 4; i++)
+        sum += findEncryptionNumber(listMiddle, 1000*i);
+
+
+    cout << "Part 2 Elapsed: " << timer.tock() / 1000.0 << endl;
+    cout << "Part 2: " << sum << endl;
+
+    for (auto& ptr : shuffleOrder)
+        delete ptr;
+    shuffleOrder.clear();
 
     return 0;
 }   
